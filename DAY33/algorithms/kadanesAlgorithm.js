59022cb42c9e7d631e5e859f5c587e3990e329a4
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-11-01 23:57:57 +0530

    Update kadanesAlgorithm.js

--- DAY33/algorithms/kadanesAlgorithm.js
+++ DAY33/algorithms/kadanesAlgorithm.js
@@ -1,1 +1,10 @@
-/
+// kadane's algorithm
+function kadanesAlgorithm(arr) {
+    let max = arr[0];
+    let maxEndingHere = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        max = Math.max(max, maxEndingHere);
+    }
+    return max;
+}
